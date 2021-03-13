303aaaddf073c24903648b474822792f8b771d2c
shane <shane.witbeck@nike.com>
2021-03-13 07:44:12 -0800

    first commit

---
+++ src/model/ci_cd.rs
@@ -0,0 +1,14 @@
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct CiCd {
+    name: String,
+    file: String,
+    link: Link,
+}
+
+impl CiCd {
+    pub fn new(name: String,
+               file: String,
+               link: Link) -> CiCd {
+        CiCd { name, file, link }
+    }
+}
